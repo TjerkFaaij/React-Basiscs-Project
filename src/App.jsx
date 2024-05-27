7a8ff8e6782199bd808d0c34e398342c1775fe63
Tjerk Faaij <tjerk.faay@gmail.com>
2024-05-27 18:36:30 +0200

    Import RecipePage change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { RecipeListPage } from "./pages/RecipeListPage";
-import { RecipePage } from "./components/RecipePage";
+import { RecipePage } from "./pages/RecipePage";
 
 export const App = () => {
   const [userRecipe, setUserRecipe] = useState();
