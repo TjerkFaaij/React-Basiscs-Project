ef45d882293e5a9f8921a905209bf4f30e617954
Tjerk Faaij <tjerk.faay@gmail.com>
2024-05-27 15:28:47 +0200

    Added a search option to RecipeListPage

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,12 +1,29 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Input } from "@chakra-ui/react";
 import { data } from "../utils/data";
 import { RecipeCard } from "../components/RecipeCard";
+import { useState } from "react";
 
 export const RecipeListPage = ({ clickFn }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredHits = data.hits.filter((hit) =>
+    hit.recipe.label.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <Box p={5}>
+    <Box p={5} bg="gray.100">
+      <Input
+        placeholder="Search for recipes"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        mb={4}
+      />
       <Flex wrap="wrap" justify="center" mx="auto">
-        {data.hits.map((hit, index) => (
+        {filteredHits.map((hit, index) => (
           <Box key={index} p={4}>
             <RecipeCard hit={hit} clickFn={clickFn} />
           </Box>
