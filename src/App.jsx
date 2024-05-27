e2a0894716da16c26882cb0abf7cdb664c2a0c3e
Tjerk Faaij <tjerk.faay@gmail.com>
2024-05-27 15:30:57 +0200

    Updated app with the RecipePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import { RecipeListPage } from './pages/RecipeListPage';
+import { useState } from "react";
+import { RecipeListPage } from "./pages/RecipeListPage";
+import { RecipePage } from "./components/RecipePage";
 
 export const App = () => {
-  // Your state code here
-  return <RecipeListPage />;
+  const [userRecipe, setUserRecipe] = useState();
+
+  return (
+    <div>
+      {userRecipe ? (
+        <RecipePage hit={userRecipe} onClick={setUserRecipe} />
+      ) : (
+        <>
+          <RecipeListPage clickFn={setUserRecipe} />;
+        </>
+      )}
+    </div>
+  );
 };
